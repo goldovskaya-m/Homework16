2b4663b7ceef1edab417fadc8808562f34d54bb3
goldovskaya-m <ariya-aaa@yandex.ru>
2024-07-28 20:46:30 +0300

    initial commit

--- src/main/java/pro/sky/skypro16/service/CalculatorService.java
+++ src/main/java/pro/sky/skypro16/service/CalculatorService.java
@@ -1,10 +1,29 @@
 package pro.sky.skypro16.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Component
 public class CalculatorService {
-    public  String sayHello() {
-        return  "<b>Hello Mariya</b>";
+    public String sayHello() {
+        return "<b>Добро пожаловать в калькулятор</b>";
     }
-    public String sayBye(){
-        return "<b>Bye</b>";
+
+    public int calculateSum(Integer num1, Integer num2) {
+        return num1 + num2;
+    }
+
+    public int calculateSubtraction(Integer num1, Integer num2) {
+        return num1 - num2;
+    }
+
+    public int calculateMultiply(Integer num1, Integer num2) {
+        return num1 * num2;
+    }
+
+    public double calculateDivision(Integer num1, float num2) {
+        return num1 / num2;
     }
 }
+
---
+++ src/test/java/pro/sky/skypro16/Skypro16ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skypro16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
